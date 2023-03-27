6b1235116d6256a5bc060aeb48e0d9d15f1754df
Sally Turutov <45124211+SallyTurutov@users.noreply.github.com>
2023-03-27 19:38:49 +0300

    Update ourProtocol.py

--- ourProtocol.py
+++ ourProtocol.py
@@ -1,15 +1,13 @@
 import random
-import torch
 
-#my files
+# my files
 from GKR import GKR
 from GKRProver import GKRProver
 from GKRVerifier import GKRVerifier
 from typing import List
 from multilinear_extension import extend_sparse
 
-
-class TensorCode():
+class TensorCode:
     def __init__(self, C_0):
         self.C = lambda x: C_0(C_0(C_0(x)))
 
@@ -35,109 +33,125 @@ class TensorCode():
         return C_x[x, y, z]
 
 
-
-class verifier():
-    def __init__(self, x, L, C : TensorCode, p):
-        self.x = x
-        self.L = L
-        self.C = C
-        self.p = p
-
-    def talk_phase1(self, C_w):
-        if self.C.local_test(C_w) is False:
-            exit('This is a cheaty prover')
-        self.C_w = C_w
-
-    def talk_phase2(self, gkr: GKR, g : List[int]):
-        gkr_verifier = GKRVerifier(gkr, g, s)
-        l = 5 # what is the range of the indexes?
-        idx = random.randint(0, l)
-
-        data = {random.randint(0, (1 << L) - 1): random.randint(0, p - 1) for _ in range(256)}
-        poly = extend_sparse(data, L, self.p)
-
-    def talk_phase3(self, raw, v, k):
-        col_check_idx = random.randint(0, k)
-
-        # Test #1
-        counter = 0
-        for i in range(k):
-            counter += v[i] * self.C_w[i, col_check_idx]
-        if counter != raw[col_check_idx]:
-            exit('This is a cheaty prover')
-
-        # Test #2
-        overall_sum = 0
-        for i in range(k):
-            overall_sum += raw[i]
-        if overall_sum != _: # _ is the value we want to check if it is equal to
-            exit('This is a cheaty prover')
+class ProofException(Exception):
+    pass
 
 
-
-class prover():
-    def __init__(self, x, w, L, C, p):
-        self.x = x
-        self.w = w
-        self.L = L
-        self.C = C
-        self.p = p
-
-    def talk_phase1(self):
-        return self.C(self.w)
-
-    def talk_phase2(self, gkr: GKR, g : List[int]):
-        gkr_proover = GKRProver(gkr)
-        A_hg, G, s = gkr_proover.initializeAndGetSum(g)
-        return A_hg, G, s
-
-    def talk_phase3(self, u, k):
-        matrix = self.C(self.w)
-        raw = torch.zeroes(matrix.size(dim=1))
-        for i in range(k):
-            raw += u[i] * matrix[i]
-        return raw
+# Phase 1 structures:
 
 
+class CodeWordVerifier:
+    def __init__(self, code):
+        self.code = code
 
-def runProtocol(x, w, L, p: int):
-    """
-        :param x:
-        :param w:
-        :param L:
-        :param p: field size
-    """
-    # need to add assert on the dimensions
+    def receive(self, codeword):
+        if self.code.local_test(codeword) is False:
+            raise ProofException('This is a cheaty prover')
 
-    C_0 = lambda x: x  # Just for now
-    C = TensorCode(C_0)
 
-    v = verifier(x, L, C, p)
-    p = prover(x, w, L, C, p)
+class CodeWordProver:
+    def __init__(self, witness, code):
+        self.codeword = code(witness)
 
-    # talk_phase1:
-    # The prover:   send to the verifier C(w)
-    # The verifier: checks it with LDT
-    C_w = p.talk_phase1()
-    v.talk_phase1(C_w)
+    def receive(self):
+        return self.codeword
 
-    # talk_phase2:
-    # The prover:   GKR
-    # The verifier: GKR
-    gkr = GKR(...); g = []
-    p.talk_phase2(gkr, g)
-    v.talk_phase2(gkr, g)
 
-    # Example of use from testGKR_Prover.py:
-    # gkr = randomGKR(L, p)
-    # g = [random.randint(0, p - 1) for _ in range(L)]
-    # pv = GKRProver(gkr)
-    # A_hg, G, s = pv.initializeAndGetSum(g)
-    # v = GKRVerifier(gkr, g, s)
-    # assert v.state == GKRVerifierState.PHASE_ONE_LISTENING, "Verifier sanity check failed"
-    # pv.proveToVerifier(A_hg, G, s, v)
-    # self.assertEqual(v.state, GKRVerifierState.ACCEPT)
-    # print(f"\b\b\b\b\b\b\bPASS")
+class CodeWordProof:
+    def __init__(self, witness, code):
+        self.verifier = CodeWordVerifier(code)
+        self.prover = CodeWordProver(witness, code)
+        self.codeword = None
 
-    raw = p.talk_phase3(u, k)  # coeff_matrix = u * v, how do we determine k?
-    v.talk_phase3(raw, v, k)
+    def prove(self):
+        self.codeword = self.prover.receive()
+        self.verifier.receive(self.codeword)
+
+
+# phase 2 structures:
+
+
+class AllZeroVerifier:
+    def __init__(self):
+        pass
+
+    def receive(self, polynomial):
+        # check the poly is zero, return random field member
+        # if this is the last phase return the indexes for phase 3
+        pass
+
+
+class AllZeroProver:
+    def __init__(self, formula, witness):
+        # calculate LDE of p
+        pass
+
+    def receive(self, constant):
+        # do the all zero calculation and return the polynomial.
+        pass
+
+
+class AllZeroProof:
+    def __init__(self, formula, witness):
+        # create verifier and prover
+        pass
+
+    def prove(self):
+        # implement the all zero proof
+        pass
+
+# Phase 3 structures:
+
+
+class LDEVerifier:
+    def __init__(self, formula, index_value_list, code, code_word):
+        # TODO: Assaf
+        pass
+
+    def receive(self):  # TODO: complete inputs
+        # TODO: Assaf
+        pass
+
+
+class LDEProver:
+    def __init__(self): # TODO: complete inputs
+        pass
+
+    def receive(self): # TODO: complete inputs
+        # TODO: Assaf
+        pass
+
+
+class LDEProof:
+    def __init__(self):  # TODO: complete inputs
+        pass
+
+    def prove(self):  # TODO: complete inputs
+        # TODO: Assaf
+        pass
+
+
+class MainProof:
+    def __init__(self, formula, witness, code):
+        # create all the verifiers provers and proofs
+        pass
+
+    def prove(self):
+        # save relevant info between the steps and run all the proofs.
+        pass
+
+
+'''
+the protocol:
+prover:                              verifier:
+                -> C(w) ->
+        ------------------------------
+          run _all zero check_ from hw2
+          on the polynomial P (from class)
+        ------------------------------
+          linearity check on C(w)
+        ------------------------------
+                                            ACK/REJ
+'''
+
+
