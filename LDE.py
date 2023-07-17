3bf0198d8a134629f19983a897328ce5a86e81f6
Sally Turutov <45124211+SallyTurutov@users.noreply.github.com>
2023-07-17 19:02:59 +0300

    Updated the lde function and added parallelism
    The function f must be defined with def (not with lambda) for it to work.

--- LDE.py
+++ LDE.py
@@ -1,48 +1,45 @@
 import numpy as np
 import functools
 import itertools
+from multiprocessing import Pool
 import galois
 from parameters import F
 
-def I0(xi,yi):
+
+def I0(xi, yi):
     if isinstance(xi, F) or isinstance(yi, F):
-        return F(1)-F(xi)-F(yi)+2*F(xi)*F(yi)
-    return 1-xi-yi+2*xi*yi
+        return F(1) - F(xi) - F(yi) + 2 * F(xi) * F(yi)
+    return 1 - xi - yi + 2 * xi * yi
+
 
 def I(x, y, H, m):
-    I_s = (I0(xi, yi) for xi,yi in itertools.zip_longest(x,y,fillvalue=0))
-    return functools.reduce(lambda a,b: a*b, I_s)
-'''    numerator, denumenator = 1, 1
-    for i in range(0, m):
-        for h in H:
-            if h == y[i]: continue #TODO: change to the form without devision
-            numerator *= (x[i]-h)
-            denumenator *= (y[i]-h)
-    return numerator / denumenator'''
+    I_s = (I0(xi, yi) for xi, yi in itertools.zip_longest(x, y, fillvalue=0))
+    return functools.reduce(lambda a, b: a * b, I_s)
 
-def lde(f, H, m, F=F):
-    '''
-        f:H^m->F
-        H is a subset of F
-        m is the number of variables of f
-        returns f_hat:F^m->F
-    '''
-    return lambda x: functools.reduce(lambda a,b: a+b, (f(*h)*I(x,h, H, m) for h in itertools.product(H, repeat=m)))
-    
-    def inner(*args):
-        lde_tmp = lambda x: functools.reduce(lambda a,b: a+b, (f(*h)*I(x,h, H, m) for h in itertools.product(H, repeat=m)))
-        if len(args) == 1:
-            return lde_tmp(*args)
-        return lde_tmp(args)
-            
-    return inner
+
+def calculate_I(args):
+    h, x, f, H, m = args
+    return f(h) * I(x, h, H, m)
+
+
+def lde(f, H, m):
+    def r(x):
+        with Pool() as pool:
+            args = [(h, x, f, H, m) for h in itertools.product(H, repeat=m)]
+            results = pool.map(calculate_I, args)
+        return functools.reduce(lambda a, b: a + b, results)
+
+    return r
 
 
 if __name__ == "__main__":
-    H=(0,1)
-    d={0:5, 1:9}
-    f=lambda x: d[x[1]]*d[x[0]]
+    H = (0, 1)
+    d = {0: 5, 1: 9}
+
+    def f(x):
+        return d[x[1]] * d[x[0]]
+
     m = 2
-    r=lde(f,H,m)
+    r = lde(f, H, m)
     for i in itertools.product(range(10), repeat=m):
-        print(f'{i=}\t{r(i)=}') 
+        print(f'{i=}\t{r(i)=}')
